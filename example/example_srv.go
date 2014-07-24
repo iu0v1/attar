807bc07c866729e8e1bfbff3e2102e19e2c446d3
Henry <cryptix@riseup.net>
2014-07-25 01:46:29 +0200

    parse templates just once

--- example/example_srv.go
+++ example/example_srv.go
@@ -9,20 +9,18 @@ import (
 )
 
 // main page
-func mainPageHandler(res http.ResponseWriter, req *http.Request) {
-	var mainPage string = `
+var mainPage = template.Must(template.New("").Parse(`
 	<html><head></head><body><center>
 	<h1 style="padding-top:15%;">HELLO!</h1>
 	</form></center></body>
-	</html>`
-	page := template.New("main")
-	page, _ = page.Parse(mainPage)
-	page.Execute(res, "")
+	</html>`))
+
+func mainPageHandler(res http.ResponseWriter, req *http.Request) {
+	mainPage.Execute(res, nil)
 }
 
 // login page
-func loginPageHandler(res http.ResponseWriter, req *http.Request) {
-	var loginPage string = `
+var loginPage = template.Must(template.New("").Parse(`
 	<html><head></head><body>
 	<center>
 	<form id="login_form" action="/login" method="POST" style="padding-top:15%;">
@@ -31,10 +29,10 @@ func loginPageHandler(res http.ResponseWriter, req *http.Request) {
 	<input type="password" placeholder="Password" name="password"><br>
 	<input type="submit" value="LOGIN">
 	</form></center></body>
-	</html>`
-	page := template.New("main")
-	page, _ = page.Parse(loginPage)
-	page.Execute(res, "")
+	</html>`))
+
+func loginPageHandler(res http.ResponseWriter, req *http.Request) {
+	loginPage.Execute(res, nil)
 }
 
 // auth provider function
